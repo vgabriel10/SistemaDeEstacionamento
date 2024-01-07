abc881110da91ee64240a4421cf2fbc01b2cfdc7
Vitor Gabriel <66925616+vgabriel10@users.noreply.github.com>
2024-01-06 23:34:28 -0300

    Adicionando plugin Datatables

---
+++ wwwroot/js/uteis.js
@@ -0,0 +1,33 @@
+﻿
+// Funções Uteis
+
+/**
+ * @param {string} idTabela - Id da tabela.
+ * @param {string} tamanhoTabela - Tamanho da tabela no seguinte formato -> 800px
+ *                                 | por padrão vem com esse valor.
+ * */
+function CarregarTabela(idTabela,tamanhoTabela = '800px'){
+    $('#'+idTabela).DataTable({
+        language: {
+            /*url: "../lib/traducao-datatables/pt_br.json"*/
+            url: "//cdn.datatables.net/plug-ins/1.11.3/i18n/pt_br.json"
+        },
+        scrollCollapse: true,
+        scrollY: tamanhoTabela
+    })
+}
+
+
+
+//Debug
+
+function RetornarUrlAtual() {
+    let diretorioAtual = window.location.pathname;
+    console.log(diretorioAtual);
+}
+
+
+
+
+
+
