93da9d01b12ed64e971b858140a703e4e2c042fc
Vitor Gabriel <66925616+vgabriel10@users.noreply.github.com>
2024-01-11 22:46:09 -0300

    Adicionando plugin jquery-mask e adicionando mascara para as placas dos veiculos

---
+++ wwwroot/js/validacoes.js
@@ -0,0 +1,60 @@
+﻿
+
+//$(function () {
+//    $('input[name=plate]').mask('AAA 0U00', {
+//        translation: {
+//            'A': {
+//                pattern: /[A-Za-z]/
+//            },
+//            'U': {
+//                pattern: /[A-Za-z0-9]/
+//            },
+//        },
+//        onKeyPress: function (value, e, field, options) {
+//            // Convert to uppercase
+//            e.currentTarget.value = value.toUpperCase();
+
+//            // Get only valid characters
+//            let val = value.replace(/[^\w]/g, '');
+
+//            // Detect plate format
+//            let isNumeric = !isNaN(parseFloat(val[4])) && isFinite(val[4]);
+//            let mask = 'AAA 0U00';
+//            if (val.length > 4 && isNumeric) {
+//                mask = 'AAA-0000';
+//            }
+//            $(field).mask(mask, options);
+//        }
+//    });
+//});
+
+// ===========
+
+function MascaraPlaca(name) {
+    $('input[name='+name+']').mask('AAA 0U00', {
+        translation: {
+            'A': {
+                pattern: /[A-Za-z]/
+            },
+            'U': {
+                pattern: /[A-Za-z0-9]/
+            },
+        },
+        onKeyPress: function (value, e, field, options) {
+            // Convert to uppercase
+            e.currentTarget.value = value.toUpperCase();
+
+            // Get only valid characters
+            let val = value.replace(/[^\w]/g, '');
+
+            // Detect plate format
+            let isNumeric = !isNaN(parseFloat(val[4])) && isFinite(val[4]);
+            let mask = 'AAA 0U00';
+            if (val.length > 4 && isNumeric) {
+                mask = 'AAA-0000';
+            }
+            $(field).mask(mask, options);
+        }
+    });
+}
+    
